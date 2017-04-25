cd47e15a4aac9c98f54583bb8b7c27cb15530658
= <=>
2017-04-25 09:49:35 +0800

    提交用户页面

---
+++ src/views/user/index.js
@@ -0,0 +1,21 @@
+/**
+ * created by zhao
+ * 2017/3/14
+ */
+import React from 'react'
+
+class User extends React.Component {
+    componentWillMount(){
+    }
+
+    componentWillUpdate(){
+    }
+    
+    render(){
+        return (
+            <div>我是user </div>
+        )
+    }
+}
+
+export default User
