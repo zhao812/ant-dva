977af81c35de70a6c117b5e4ff5d01767717faf7
= <=>
2017-05-12 09:14:18 +0800

    修改mock数据

--- app/components/fetch/index.js
+++ app/components/fetch/index.js
@@ -1,39 +1,39 @@
 import { Modal, Button } from 'antd';
-var HTTPUtil = {};  
-  import 'whatwg-fetch'  // 可以引入fetch来进行Ajax
+var HTTPUtil = {};
+import 'whatwg-fetch'  // 可以引入fetch来进行Ajax
 /** 
  * 基于 fetch 封装的 GET请求 
  * @param url 
  * @param params {} 
  * @param headers 
  * @returns {Promise} 
- */  
-export function fetchGet (url, params, headers) {
+ */
+export function fetchGet(url, params, headers) {
     return (dispatch, getState) => {
-        if (params) {  
-            let paramsArray = [];  
+        if (params) {
+            let paramsArray = [];
             //encodeURIComponent  
-            Object.keys(params).forEach(key => paramsArray.push(key + '=' + params[key]))  
-            if (url.search(/\?/) === -1) {  
-                url += '?' + paramsArray.join('&')  
-            } else {  
-                url += '&' + paramsArray.join('&')  
-            }  
+            Object.keys(params).forEach(key => paramsArray.push(key + '=' + params[key]))
+            if (url.search(/\?/) === -1) {
+                url += '?' + paramsArray.join('&')
+            } else {
+                url += '&' + paramsArray.join('&')
+            }
         }
 
-        if(process.env.NODE_ENV == "develop"){
+        if (process.env.NODE_ENV == "develop") {
             url = "mock/" + url + ".json"
         }
 
-        return fetch(url, {  
-            method: 'GET',  
-            headers: headers,  
-        })  
-        .then((response) => {  
+        return fetch(url, {
+            method: 'GET',
+            headers: headers,
+        })
+        .then((response) => {
             return response.json();
-        })  
-        .then((data) => {  
-            if(data&&data.code!=0){
+        })
+        .then((data) => {
+            if (data && data.code != 0) {
                 Modal.error({
                     title: '提示',
                     content: data.message,
@@ -41,39 +41,44 @@ export function fetchGet (url, params, headers) {
                 return false;
             }
             return data.result;
-        })  
+        })
     }
-    
-}  
-  
-  
+
+}
+
+
 /** 
  * 基于 fetch 封装的 POST请求  FormData 表单数据 
  * @param url 
  * @param formData   
  * @param headers 
  * @returns {Promise} 
- */  
-HTTPUtil.post = function(url, formData, headers) {  
-    return new Promise(function (resolve, reject) {
-        
-      fetch(url, {  
-            method: 'POST',  
-            headers: headers,  
-            body:formData,  
-          })  
-          .then((response) => {
-              if (response.ok) {  
-                  return response.json();  
-              } else {  
-                  reject({status:response.status})  
-              }  
-          })  
-          .then((response) => {  
-              resolve(response);  
-          })  
-          .catch((err)=> {  
-            reject({status:-1});  
-          })  
-    })  
+ */
+export function fetchPost(url, formData, headers) {
+
+    return (dispatch, getState) => {
+
+        if (process.env.NODE_ENV == "develop") {
+            url = "mock/" + url + ".json"
+        }
+
+        return fetch(url, {
+                method: 'POST',
+                headers: headers,
+                body: formData,
+            })
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                if (data && data.code != 0) {
+                    Modal.error({
+                        title: '提示',
+                        content: data.message,
+                    });
+                    return false;
+                }
+                return data.result;
+            })
+    }
 }  
--- app/view/login/reducer/action.js
+++ app/view/login/reducer/action.js
@@ -5,8 +5,13 @@ const receiveData = data => ({
     type: ActionType.HOME_UPDATE,
     data: data
 })
+
 //进入加载数据
-export const getUserNumber = () => dispatch => {
-    let url = "/mock/userNumber.json";
-    dispatch(HTTPUtil.fetchGet(url, null, null)).then(data=>dispatch(receiveData(data)))
+export const userLogin = (userName, password) => dispatch => {
+    let url = "/index/login";
+    let opt = {
+        username: userName,
+        password: password
+    }
+    dispatch(HTTPUtil.fetchPost(url, opt, null)).then(data=>dispatch(receiveData(data)))
 }
